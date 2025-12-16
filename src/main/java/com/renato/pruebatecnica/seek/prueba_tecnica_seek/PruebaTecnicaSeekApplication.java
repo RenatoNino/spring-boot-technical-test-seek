c65c4bc331706850e0d75c6af27a55cdbf4500d6
Renato Niño <jorgenino07@gmail.com>
2025-12-15 20:16:55 -0500

    Initial commit - Spring Boot technical test

---
+++ src/main/java/com/renato/pruebatecnica/seek/prueba_tecnica_seek/PruebaTecnicaSeekApplication.java
@@ -0,0 +1,13 @@
+package com.renato.pruebatecnica.seek.prueba_tecnica_seek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaTecnicaSeekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaTecnicaSeekApplication.class, args);
+	}
+
+}
